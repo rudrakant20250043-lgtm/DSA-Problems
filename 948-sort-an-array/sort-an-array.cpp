116f54b4f32d885cf65857780018459537dbef03
RUDRAKANT AMARNATH MISHRA <rudrakant.20250043@mnnit.ac.in>
2026-09-22 19:35:29 +0530

    Time: 2912 ms (5.03%) | Memory: 70.8 MB (95.55%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,60 +1,31 @@
 class Solution {
 public:
 
-    void merge(vector<int>& arr, int low, int mid, int high) {
+int partition(vector<int>&arr,int low,int high){ 
+     int pivotIndex = low + rand() % (high - low + 1);
 
-        vector<int> temp;
-
-        int i = low;
-        int j = mid + 1;
-
-        while(i <= mid && j <= high) {
-
-            if(arr[i] <= arr[j]) {
-                temp.push_back(arr[i]);
-                i++;
-            }
-            else {
-                temp.push_back(arr[j]);
-                j++;
-            }
-        }
-
-        while(i <= mid) {
-            temp.push_back(arr[i]);
+        swap(arr[pivotIndex], arr[high]);
+    int pivot=arr[high];
+    int i=low-1;
+    for(int j=low;j<=high;j++){
+        if(arr[j]<pivot){
             i++;
-        }
-
-        while(j <= high) {
-            temp.push_back(arr[j]);
-            j++;
-        }
-
-        for(int k = low; k <= high; k++) {
-            arr[k] = temp[k - low];
-        }
+             swap(arr[i],arr[j]);
+        } 
+       
     }
-
-
-    void mergeSort(vector<int>& arr, int low, int high) {
-
-        if(low >= high)
-            return;
-
-        int mid = low + (high - low) / 2;
-
-        mergeSort(arr, low, mid);
-
-        mergeSort(arr, mid + 1, high);
-
-        merge(arr, low, mid, high);
-    }
-
-
-    vector<int> sortArray(vector<int>& nums) {
-
-        mergeSort(nums, 0, nums.size() - 1);
-
+    swap(arr[i+1],arr[high]);
+    return i+1;
+}
+    void quicksort(vector<int>&arr,int low,int high) {
+        if(low>=high)
+        return;
+        int p=partition(arr,low,high);
+        quicksort(arr,low,p-1);
+        quicksort(arr,p+1,high);
+    } 
+    vector<int> sortArray(vector<int>&nums){
+        quicksort(nums,0,nums.size()-1);
         return nums;
     }
 };
